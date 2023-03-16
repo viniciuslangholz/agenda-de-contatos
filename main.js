e6a6b30de913ed76c0f3e9ad146cc97493526bad
Vinícius Langholz <viniciuslangholz@gmail.com>
2023-03-16 20:59:45 -0300

    Adicionado estilização e lógica da aplicação

--- main.js
+++ main.js
@@ -10,3 +10,47 @@ const phoneMask = (value) => {
   value = value.replace(/(\d)(\d{4})$/,"$1-$2")
   return value
 }
+
+const form = document.getElementById('formulario');
+const nomes = [];
+const telefones = [];
+const emails = [];
+
+let linhas ='';
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  adicionaLinha();
+  atualizaTabela();
+});
+
+function adicionaLinha() {
+  const inputNome = document.getElementById('nome');
+  const inputTelefone = document.getElementById('telefone');
+  const inputEmail = document.getElementById('email');
+
+  if (nomes.includes(inputNome.value)) {
+    alert(`O contato ${inputNome.value} já existe. Escolha outro nome.`);
+  } else {
+    nomes.push(inputNome.value);
+    telefones.push(inputTelefone.value);
+    emails.push(inputEmail.value);
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += `<td>${inputEmail.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+  }
+
+  inputNome.value = '';
+  inputTelefone.value = '';
+  inputEmail.value = '';
+}
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+}
